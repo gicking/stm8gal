f8df6192b08c3b43d8ac62d033649a9f199721bb
gicking <icking@onlinehome.de>
2018-12-04 21:21:54 +0100

    minor changes in shell scripts
    minor changes in shell scripts

--- _flash_dummy.sh
+++ _flash_dummy.sh
@@ -5,23 +5,31 @@
 # change to current working directory
 cd `dirname $0`
 
-# set path to STM flash loader, serial port name, etc.
+# set path to flash loader
 LOADER=./stm8gal
-PORT=/dev/ttyUSB0
+
+# set other parameters
 FIRMWARE=./dummy.s19
 #FIRMWARE=./dummy.txt
 
-# acccording to STM8 bootloader manual section 2.1, the minimum baudrate is 4800Baud
-#BAUD=4800
-#BAUD=9600
-#BAUD=19200
-BAUD=38400
-#BAUD=57600
-#BAUD=115200
-#BAUD=230400
+# set serial port
+PORT=/dev/ttyUSB0
+
+# set optional UART mode (0=duplex, 1=1-wire reply, 2=2-wire reply)
+#MODE=2
+
+# acccording to STM8 bootloader manual section 2.1, the minimum baudrate is 4800Baud. Default is 19200Baud
+# Typical baudrates are: 4800 9600 19200 38400 57600 115200 230400
+BAUD=230400
+
+# if not explicitely set, configure auto-detect mode
+if [ -z "$MODE" ]; then MODE=255; fi
+
+# for UART reply mode (MODE=1|2) limit baudrate. Found >19.2Baud is unreliable
+if [[ $MODE != 0 ]]; then BAUD=19200; fi
 
 # use flash loader to upload new SW
-$LOADER -p $PORT -b $BAUD -w $FIRMWARE -v -V 2
+$LOADER -p $PORT -b $BAUD -u $MODE -w $FIRMWARE -v -V 2
 
 echo
 read -p "press return to close window"
--- _memory_dump.sh
+++ _memory_dump.sh
@@ -5,24 +5,32 @@
 # change to current working directory
 cd `dirname $0`
 
-# set path to STM flash loader, serial port name, etc.
+# set path to flash loader
 LOADER=./stm8gal
-PORT=/dev/ttyUSB0
+
+# set other parameters
 ADDRSTART=0x8000
 ADDRSTOP=0x80FF
 OUTFILE=./dump.txt
 
-# acccording to STM8 bootloader manual section 2.1, the minimum baudrate is 4800Baud
-#BAUD=4800
-#BAUD=9600
-#BAUD=19200
-BAUD=38400
-#BAUD=57600
-#BAUD=115200
-#BAUD=230400
+# set serial port
+PORT=/dev/ttyUSB0
+
+# set optional UART mode (0=duplex, 1=1-wire reply, 2=2-wire reply)
+#MODE=2
+
+# acccording to STM8 bootloader manual section 2.1, the minimum baudrate is 4800Baud. Default is 19200Baud
+# Typical baudrates are: 4800 9600 19200 38400 57600 115200 230400
+BAUD=230400
+
+# if not explicitely set, configure auto-detect mode
+if [ -z "$MODE" ]; then MODE=255; fi
+
+# for UART reply mode (MODE=1|2) limit baudrate. Found >19.2Baud is unreliable
+if [[ $MODE != 0 ]]; then BAUD=19200; fi
 
 # use flash loader to read out memory
-$LOADER -p $PORT -b $BAUD -r $ADDRSTART $ADDRSTOP $OUTFILE -V 2
+$LOADER -p $PORT -b $BAUD -u $MODE -r $ADDRSTART $ADDRSTOP $OUTFILE -V 2
 
 echo
 read -p "press return to close window"
