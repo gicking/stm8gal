a75218b356e17682296c459367e0f6b40dcb7b0d
gicking <icking@onlinehome.de>
2018-12-07 02:24:27 +0100

    clean-up DOS batchfiles
    clean-up DOS batchfiles

--- _flash_dummy.bat
+++ _flash_dummy.bat
@@ -11,22 +11,20 @@ set FIRMWARE=".\dummy.s19"
 REM set FIRMWARE=".\dummy.txt"
 
 REM set serial port
-REM set PORT=10
-REM set PORT=15
-set PORT=10
+set PORT=11
 
-REM set optional UART mode (0=duplex, 1=1-wire reply, 2=2-wire reply)
-REM set MODE=2
+REM set optional UART mode (0=duplex, 1=1-wire reply, 2=2-wire reply, else auto-detect)
+REM set MODE=0
 
-REM acccording to STM8 bootloader manual section 2.1, the minimum baudrate is 4800Baud. Default is 19200Baud
+REM acccording to STM8 bootloader manual section 2.1, the minimum baudrate is 4800Baud. Default is 115.2kBaud
 REM Typical baudrates are: 4800 9600 19200 38400 57600 115200 230400
-set BAUD=230400 
+set BAUD=230400
 
 REM if not explicitely set, configure auto-detect mode
 IF NOT DEFINED MODE set MODE=255
 
-REM for UART reply mode (MODE=1|2) limit baudrate. Found >19.2Baud is unreliable
-IF NOT %MODE% == 0  set BAUD=19200
+REM limit UART reply mode (MODE=1,2) to <=115.2kBaud
+IF NOT %MODE% == 0  set BAUD=115200
 
 REM use flash loader to upload new SW
 %LOADER% -p COM%PORT% -b %BAUD% -u %MODE% -w %FIRMWARE% -v -V 2
