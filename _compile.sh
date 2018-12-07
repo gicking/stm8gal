e4245a6174882b3b190cb80deed37f0ad456642d
gicking <icking@onlinehome.de>
2018-12-07 02:24:03 +0100

    double-click opens terminal window
    double-click opens terminal window

--- _compile.sh
+++ _compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
+# open terminal on double-click, skip else. See https://www.linuxquestions.org/questions/programming-9/executing-shell-script-in-terminal-directly-with-a-double-click-370091/
+tty -s; if [ $? -ne 0 ]; then /etc/alternatives/x-terminal-emulator -e "$0"; exit; fi
+
 # change to current working directory
 cd `dirname $0`
 
