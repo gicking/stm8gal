e0a5f5fe1fd39ab74e5805adebb163334d1397be
Georg Icking-Konert <icking@onlinehome.de>
2018-12-20 13:02:56 +0100

    removed obsolete scripts
    removed obsolete scripts. Can be easily done from Explorer and context
    menu

--- _open_terminal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-# change to current working directory
-cd `dirname $0`
-
-# open terminal, see https://askubuntu.com/questions/640096/how-do-i-check-which-terminal-i-am-using
-/etc/alternatives/x-terminal-emulator
